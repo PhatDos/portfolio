7582375f7babbfee115e2fd2cc9ec63ee6d835f4
PhatDos <162654433+PhatDos@users.noreply.github.com>
2025-09-13 11:05:00 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+// import Home from './pages/Home'
 import Portfolio from './pages/Porfolio'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Portfolio />} />
       <Route path="/portfolio" element={<Portfolio />} />
     </Routes>
   )
